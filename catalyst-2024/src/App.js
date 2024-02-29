8bc1250824a92e0145a62ea26beea5e4ce33e8bf
Rohit Prasanna <rohit.prasanna1@gmail.com>
2024-02-29 13:46:42 -0500

    Created base React.js project with Tailwind CSS

---
+++ catalyst-2024/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import FirstComponent from './components/FirstComponent.jsx'
+
+
+function App() {
+  return (
+    <div className="App">
+      <FirstComponent/>
+    </div>
+  );
+}
+
+export default App;
