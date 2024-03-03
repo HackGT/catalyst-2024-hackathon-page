8767b2e64a853bce94f8eeee0d85455f8f6995ca
Blueflame100 <ishanpanchamia@gmail.com>
2024-03-03 15:24:33 -0500

    Fixed smooth scrolling functionality

--- catalyst-2024/src/components/main_page/Header.jsx
+++ catalyst-2024/src/components/main_page/Header.jsx
@@ -6,11 +6,11 @@ const Header = () => {
         <nav className="fixed w-full z-50 hidden md:flex h-navBar bg-headerTeal py-4">
             <div className="container mx-auto px-4">
                 <ul className="flex justify-between">
-                    <li><a href="/" className="text-navText text-teal-900 font-coco hover:text-teal-600">HOME</a></li>
-                    <li><a href="#about" className="text-navText text-teal-900 font-coco hover:text-teal-600">ABOUT</a></li>
-                    <li><a href="#tracks" className="text-navText text-teal-900 font-coco hover:text-teal-600">TRACKS</a></li>
-                    <li><a href="#schedule" className="text-navText text-teal-900 font-coco hover:text-teal-600">SCHEDULE</a></li>
-                    <li><a href="#faq" className="text-navText text-teal-900 font-coco hover:text-teal-600">FAQ</a></li>
+                    <li><Link to="front" smooth={true} duration={500} className="text-navText text-teal-900 font-coco hover:text-teal-600">HOME</Link></li>
+                    <li><Link to="about" smooth={true} duration={500} className="text-navText text-teal-900 font-coco hover:text-teal-600">ABOUT</Link></li>
+                    <li><Link to="tracks" smooth={true} duration={500} className="text-navText text-teal-900 font-coco hover:text-teal-600">TRACKS</Link></li>
+                    <li><Link to="schedule" smooth={true} duration={500} className="text-navText text-teal-900 font-coco hover:text-teal-600">SCHEDULE</Link></li>
+                    <li><Link to="faq" smooth={true} duration={500} className="text-navText text-teal-900 font-coco hover:text-teal-600">FAQ</Link></li>
                 </ul>
             </div>
         </nav>
