f7c32b2c76e9941511e7ab9efe780d1a3233cdb3
Rohit Prasanna <rohit.prasanna1@gmail.com>
2024-03-06 23:35:52 -0500

    Font size fixes; commented out current backend implementation for future integration

--- catalyst-2024/src/components/information/Schedule.jsx
+++ catalyst-2024/src/components/information/Schedule.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ScheduleSection = () => {
     return (
         <div id="schedule" className="relative p-10 text-center">
-            <h2 className="text-5xl text-textColorDark font-coco font-bold mb-3">SCHEDULE - COMING SOON!</h2>
+            <h2 className="text-6xl text-textColorDark font-coco font-bold mb-3">SCHEDULE - COMING SOON!</h2>
             <p className="text-3xl italic font-bubblegum">
                 *subject to change as details arrive <br /> and will be updated accordingly
             </p>
