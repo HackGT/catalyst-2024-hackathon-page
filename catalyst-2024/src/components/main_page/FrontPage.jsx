a367b69ecb29a919c11c00a0a82b8ccb1ab03878
Blueflame100 <ishanpanchamia@gmail.com>
2024-03-12 19:47:43 -0400

    Added leaf images

--- catalyst-2024/src/components/main_page/FrontPage.jsx
+++ catalyst-2024/src/components/main_page/FrontPage.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 // import ShellImage from '../../images/Shell.svg';
 // import SeaweedImage from '../../images/Seaweed.svg';
 // import SeashellImage from '../../images/Seashell.svg';
+import SeaweedLeft from '../../images/SeaweedLeft.svg';
+import SeaweedRight from '../../images/SeaweedRight.svg';
 import BeachConversion from '../../images/waves.svg'
 
 const FrontPage = () => {
@@ -12,6 +14,8 @@ const FrontPage = () => {
                 <img src={SeaweedImage} alt="Seaweed" className="h-8" />
                 <img src={SeashellImage} alt="Seashell" className="h-8" /> */}
             </div>
+            <img src={SeaweedLeft} alt="Seaweed Left" className="hidden md:flex absolute left-0 top-1/2 transform -translate-y-1/2 h-auto w-auto" />
+            <img src={SeaweedRight} alt="Seaweed Right" className="hidden md:flex absolute right-0 top-1/6 transform -translate-y-1/3 h-auto w-auto" />
             <div className="font-coco text-center pb-24 p-4">
                 <h1 className="text-6xl md:text-9xl text-white text-outline-small md:text-outline-large font-bold pb-3">CATALYST</h1>
                 <p className="text-3xl md:text-5xl text-headerBlue">DIVE INTO LEARNING</p>
