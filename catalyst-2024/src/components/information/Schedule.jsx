f53b44b1aaed33dfdfad98488bfb323d43e793f5
Rohit Prasanna <rohit.prasanna1@gmail.com>
2024-03-28 17:39:15 -0400

    Centered schedules propely

--- catalyst-2024/src/components/information/Schedule.jsx
+++ catalyst-2024/src/components/information/Schedule.jsx
@@ -10,34 +10,28 @@ import Schedule3 from '../../images/Schedule3.svg';
 const ScheduleSection = () => {
     // Settings for the carousel
     const settings = {
-        dots: true, // Show dot indicators at the bottom
-        infinite: true, // Infinite looping
-        speed: 500, // Transition speed
-        slidesToShow: 1, // Number of slides to show at a time
-        slidesToScroll: 1, // Number of slides to scroll on each swipe
-        autoplay: false, // Enable autoplay
-        // autoplaySpeed: 3000, // Speed at which to autoplay
+        dots: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000, // Speed at which to autoplay
     };
 
     return (
         <div id="schedule" className="relative p-10 text-center">
-            <h2 className="text-3xl md:text-6xl text-textColorDark font-coco font-bold mb-3">SCHEDULE - COMING SOON!</h2>
-            <p className="text-xl md:text-3xl italic font-bubblegum">
-                *subject to change as details arrive <br /> and will be updated accordingly
-            </p>
-            {/* Carousel */}
+            <h2 className="text-3xl md:text-6xl text-textColorDark font-coco font-bold mb-3">SCHEDULE</h2>
             <div id="slider">
-                <Slider {...settings} className='h-screen flex items-center justify-center'>
+                <Slider {...settings} className='flex items-center justify-center'>
                     <div className='h-screen flex items-center justify-center'>
-                        <img src={Schedule1} alt="Schedule1" className='h-screen flex items-center justify-center'/>
+                        <img src={Schedule1} alt="Schedule1" className='mx-auto' />
                     </div>
-                    <div>
-                        <img src={Schedule2} alt="Schedule2" />
+                    <div className='h-screen flex items-center justify-center'>
+                        <img src={Schedule2} alt="Schedule2" className='mx-auto' />
                     </div>
-                    <div>
-                        <img src={Schedule3} alt="Schedule3" />
+                    <div className='h-screen flex items-center justify-center'>
+                        <img src={Schedule3} alt="Schedule3" className='mx-auto' />
                     </div>
-                    {/* Add more slides as needed */}
                 </Slider>
             </div>
             
