a1481b8e68d834683167fa0d7bed8d7ed07f8bca
Soham Gunturu <gunturu.soham@gmail.com>
2025-02-23 17:50:22 -0500

    Update text to match figma design

--- src/components/faq/AskQuestion.jsx
+++ src/components/faq/AskQuestion.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 // import axios from 'axios';
 
@@ -53,6 +54,31 @@ const AskQuestionForm = () => {
       </p>
     </div>
   );
+=======
+import React from 'react';
+import OceanLayersImage from '../../images/OceanLayers.svg';
+
+const AskQuestionForm = () => {
+    return (
+        <div className="text-white text-center relative">
+            <img src={OceanLayersImage} alt="OceanLayers" className="w-full"></img>
+
+            <h2 className="text-2xl md:text-4xl font-bubblegum mb-4 absolute top-8 left-1/2 transform -translate-x-1/2 text-white font-bold">
+                Finish!
+            </h2>
+            <h2 className="text-xl md:text-3xl font-bubblegum mb-4 absolute top-24 left-1/2 transform -translate-x-1/2 text-white">
+                Other Questions?
+            </h2>
+            <h2 className="text-xl md:text-3xl font-bubblegum absolute top-36 left-1/2 transform -translate-x-1/2 text-white">
+                Email us at hello@hexlabs.org!
+            </h2>
+
+            <p className="absolute text-xs md:text-3xl font-bubblegum bottom-5 left-1/2 transform -translate-x-1/2">
+                WITH ❤️ FROM THE HEXLABS TEAM
+            </p>
+        </div>
+    );
+>>>>>>> f54d565 (Update text to match figma design)
 };
 
 export default AskQuestionForm;
