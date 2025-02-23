d65367075248cdaf052075374f3e3cc884deb3cd
vingupta22 <vguptax1@gmail.com>
2025-02-23 11:59:50 -0500

    changed colors to match new style

--- src/components/faq/AskQuestion.jsx
+++ src/components/faq/AskQuestion.jsx
@@ -61,21 +61,34 @@ import OceanLayersImage from '../../images/OceanLayers.svg';
 const AskQuestionForm = () => {
     return (
         <div className="text-white text-center relative">
-            <img src={OceanLayersImage} alt="OceanLayers" className="w-full"></img>
 
-            <h2 className="text-2xl md:text-4xl font-bubblegum mb-4 absolute top-8 left-1/2 transform -translate-x-1/2 text-white font-bold">
-                Finish!
-            </h2>
-            <h2 className="text-xl md:text-3xl font-bubblegum mb-4 absolute top-24 left-1/2 transform -translate-x-1/2 text-white">
-                Other Questions?
-            </h2>
-            <h2 className="text-xl md:text-3xl font-bubblegum absolute top-36 left-1/2 transform -translate-x-1/2 text-white">
-                Email us at hello@hexlabs.org!
-            </h2>
+            <h2 className="text-xl md:text-3xl font-bubblegum mb-4 absolute top-10 left-1/2 transform -translate-x-1/2 text-white">Other Questions?</h2>
+            <h2 className="text-xl md:text-3xl font-bubblegum absolute top-20 left-1/2 transform -translate-x-1/2 text-white">Email us at hello@hexlabs.org!</h2>
 
-            <p className="absolute text-xs md:text-3xl font-bubblegum bottom-5 left-1/2 transform -translate-x-1/2">
-                WITH ❤️ FROM THE HEXLABS TEAM
-            </p>
+            {/* <form className="max-w-md mx-auto absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <div className="mb-4">
+                    <input
+                        type="email"
+                        placeholder="Your email address"
+                        className="w-full p-2 rounded text-blue-900"
+                        required
+                    />
+                </div>
+                <div className="mb-4">
+                    <textarea
+                        placeholder="Your question"
+                        className="w-full p-2 rounded text-blue-900"
+                        rows="4"
+                        required
+                    ></textarea>
+                </div>
+                <button onClick={sendEmail} type="submit" className="bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded">
+                    Send Question
+                </button>
+            </form> */}
+            <p className="text-xs md:text-3xl font-bubblegum text-center mt-8">
+    WITH 💙 FROM THE HEXLABS TEAM
+</p>
         </div>
     );
 >>>>>>> f54d565 (Update text to match figma design)
