5590f5b27ffd74ee4a4459f986c8b7eaaba737c8
lkang172 <kangzundao@gmail.com>
2025-03-08 16:12:32 -0500

    added car svg

--- src/components/main_page/FrontPage.jsx
+++ src/components/main_page/FrontPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import FrontImage from "../../images/frontpage.svg";
+import Car from "../../images/car.svg";
 
 const FrontPage = () => {
   return (
@@ -9,6 +10,11 @@ const FrontPage = () => {
         src={FrontImage}
         alt="Front Image"
         className="hidden lg:flex absolute left-1/8 top-0 h-screen w-100 z-0"
+      />
+      <img
+        src={Car}
+        alt="Racecar"
+        className="hidden lg:flex absolute top-40p left-15p h-auto w-auto z-0"
       />
       <div className="font-racesport text-center pb-24 p-4 relative z-50 h-screen">
         <h1 className="text-5xl md:text-8xl text-white  pb-3 uppercase">
