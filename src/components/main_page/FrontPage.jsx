8e823f6c9d684d67ec2ce8dd8d672b70766697e4
Soham Gunturu <gunturu.soham@gmail.com>
2025-03-10 19:39:34 -0400

    Add hifi components

--- src/components/main_page/FrontPage.jsx
+++ src/components/main_page/FrontPage.jsx
@@ -1,26 +1,33 @@
 import React from "react";
-import FrontImage from "../../images/frontpage.svg";
-import Car from "../../images/car.svg";
+//import FrontImage from "../../images/frontpage.svg";
+import Car from "../../images/hificar.png";
+import Bear from "../../images/bear.png";
+import FrontTrack from "../../images/frontTrack.png";
 
 const FrontPage = () => {
   return (
-    <div className={`home bg-bgcustom text-center pt-10 md:pt-44`}>
+    <div className={`home bg-bgcustom text-center pt-10 md:pt-20`}>
       <div className="relative justify-center items-center space-x-2 mb-4"></div>
       <img
-        src={FrontImage}
+        src={FrontTrack}
         alt="Front"
         className="hidden lg:flex absolute left-1/8 top-0 h-screen w-100 z-0"
       />
       <img
         src={Car}
         alt="Racecar"
-        className="hidden lg:flex absolute top-40p left-15p h-auto w-auto z-0"
+        className="hidden lg:flex absolute top-[55%] left-15p h-auto w-auto z-0"
+      />
+      <img
+        src={Bear}
+        alt="Bear Driver"
+        className="hidden lg:flex absolute top-[55%] left-[44%] h-46 w-46 z-5"
       />
       <div className="font-racesport text-center pb-24 p-4 relative z-50 h-screen">
-        <h1 className="text-5xl md:text-8xl text-white  pb-3 uppercase">
+        <h1 className="text-5xl md:text-8xl text-white pb-3 uppercase">
           CATALYST 2025
         </h1>
-        <p className="text-2xl md:text-4xl text-white font-jetbrains">
+        <p className="text-2xl md:text-4xl text-white font-jetbrains pb-32">
           Start Your Engines!
         </p>
       </div>
