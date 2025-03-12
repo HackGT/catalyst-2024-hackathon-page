9c37b736fafeab594b72c2f363395f47d0d3e4c8
jojobaest <joiewqyeung@gmail.com>
2025-03-12 01:48:23 -0400

    Hifi FAQ section updated

--- src/components/faq/FAQItem.jsx
+++ src/components/faq/FAQItem.jsx
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState } from 'react';
+import UpArrow from '../../images/up-arrow.svg';
+import DownArrow from '../../images/down-arrow.svg';
 
 const FAQItem = ({ question, answer, link }) => {
-  return (
-    <div className="w-custom mx-auto my-2 p-4 bg-white text-bgcustom rounded-lg shadow-md">
-      <h3 className="text-xl md:text-xl font-jetbrains">{question}</h3>
-      <p className="mt-1 text-md md:text-xl font-jetbrains">
-        {answer}
-        {link && (
-          <a href={link.url} className="text-blue-500 hover:text-blue-600">
-            {link.text}
-          </a>
-        )}
-      </p>
-    </div>
-  );
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <div className="w-full mx-auto my-2 bg-[#C6C6C6] text-bgcustom rounded-[20px] shadow-md">
+            <button 
+                className="text-[24px] bg-[#FFFFFF] text-[#000000] p-4 px-8 w-full flex items-center justify-between text-left font-jetbrains font-bold tracking-wider rounded-[20px]"
+                onClick={() => setIsOpen(!isOpen)}
+            >
+                {question}
+                <img 
+                    src={isOpen ? UpArrow : DownArrow} 
+                    alt="Toggle Icon" 
+                    className="h-4 w-6 transition-transform duration-300"
+                />
+            </button>
+            {isOpen && (
+                <p className="text-[20px] text-[#000000] p-4 px-8 mt-1 font-jetbrains bg-grey font-bold text-left tracking-wider">
+                    {answer}
+                    {link && (
+                        <a href={link.url} className="text-blue-500 hover:text-blue-600">{link.text}</a>
+                    )}
+                </p>
+            )}
+        </div>
+    );
 };
 
 export default FAQItem;
--- src/components/faq/FAQList.jsx
+++ src/components/faq/FAQList.jsx
@@ -1,53 +1,65 @@
-import React from "react";
-import FAQItem from "./FAQItem";
+import React from 'react';
+import FAQItem from './FAQItem';
+import BottomTire from '../../images/Bottom-tire.svg';
+import TopTire from '../../images/Top-tire.svg';
+import LeftCone from '../../images/Left-cone.svg';
+import RightCone from '../../images/Right-cone.svg';
 
 const FAQList = () => {
-  const faqs = [
-    {
-      question: "Who can attend?",
-      answer:
-        "High school students from all across the nation are welcome to register.",
-      link: null,
-    },
-    {
-      question: "Where is Catalyst Hacks?",
-      answer: "The hackathon will be fully virtual! You can register at ",
-      link: {
-        url: "https://registration.hexlabs.org/67033be1d1c52afadcfb2968",
-        text: "this link.",
-      },
-    },
-    {
-      question: "How do I choose which track to attend?",
-      answer:
-        "Your project should adhere to one of the three tracks. You can either create an idea based off of these tracks or submit to whichever one fits your project the best.",
-      link: null,
-    },
-    {
-      question: "What is HexLabs?",
-      answer:
-        "HexLabs is a student organization and 501(c)(3) non-profit at Georgia Tech devoted to Computer Science outreach in collegiate and high school communities. To learn more, check out our organization website ",
-      link: { url: "https://hexlabs.org", text: "here." },
-    },
-  ];
+    const faqs = [
+        {
+            question: 'What is HexLabs?',
+            answer: 'HexLabs is a student organization and 501(c)(3) non-profit at Georgia Tech devoted to Computer Science outreach in collegiate and high school communities. To learn more, check out our organization website ',
+            link: { url: 'https://hexlabs.org', text: 'here.' },
+        },
+        {
+            question: 'Who can attend?',
+            answer: 'High school students from all across Georgia are welcome to register.',
+            link: null,
+        },
+        {
+            question: 'Where is Catalyst?',
+            answer: "We'll be waiting for you at the Instructional Center[link google maps]",
+            link: { url: null},
+        },
+        {
+            question: 'How do I choose which track to attend?',
+            answer: 'During registration, students will be allowed to choose which of the three tracks they\'d like to attend. For more information regarding each option, see either the Tracks section above or reference the registration application.',
+            link: null,
+        },
+        {
+            question: 'Do I need to bring anything to the idea-thon?',
+            answer: 'We will provide food for every participant. Participants are expected to bring their own laptops, if necessary, we can provide a limited amount.',
+            link: null,
+        }
+    ];
 
-  return (
-    <div className="faq p-8">
-      <h2 className="text-3xl md:text-6xl font-racesport text-textColor text-center mb-8">
-        PIT STOP!! (FAQ)
-      </h2>
-      <div className="space-y-4">
-        {faqs.map((faq, index) => (
-          <FAQItem
-            key={index}
-            question={faq.question}
-            answer={faq.answer}
-            link={faq.link}
-          />
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className="faq p-8 bg-[#0F0D2E]">
+            <div className="flex justify-between items-center mb-8 pb-8">
+                <div className="flex-1 flex justify-center">
+                    <img src={LeftCone} alt="Left" className="w-50" />
+                </div>
+                <div className="flex-1 flex justify-center">
+                    <h2 className="text-white text-[40px] font-racesport leading-[40px] uppercase transform translate-y-10">PIT STOP!! (FAQ)</h2>
+                </div>
+                <div className="flex-1 flex justify-center">
+                    <div className="flex flex-col items-center">
+                        <div className="flex items-center">
+                            <img src={BottomTire} alt="Tire" className="w-45 -mb-20 z-10 -mr-8 transform translate-x-10" />
+                            <img src={TopTire} alt="Tire" className="w-30 -mt-20 -ml-20 z-10 transform translate-x-10 translate-y-6" />
+                            <img src={RightCone} alt="Cone" className="w-50 -ml-10 z-0" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-col items-start gap-[64px] w-[1183px] mx-auto">
+                {faqs.map((faq, index) => (
+                    <FAQItem key={index} question={faq.question} answer={faq.answer} link={faq.link} />
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default FAQList;
