b7a66d93a5ea0f0a6e8b745652fdbccec320efa5
lkang172 <kangzundao@gmail.com>
2025-03-12 15:02:26 -0400

    finished footer, removed "with love from hexlabs" from askquestion.jsx

--- src/components/faq/AskQuestion.jsx
+++ src/components/faq/AskQuestion.jsx
@@ -1,13 +1,7 @@
 import React from "react";
 
 const AskQuestionForm = () => {
-  return (
-    <div className="text-white text-center relative">
-      <p className="absolute text-xs md:text-3xl font-racesport left-1/2 transform -translate-x-1/2 translate-y-4">
-        WITH 💙 FROM THE HEXLABS TEAM
-      </p>
-    </div>
-  );
+  return <div className="text-white text-center relative"></div>;
 };
 
 export default AskQuestionForm;
